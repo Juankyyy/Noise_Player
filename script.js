f882748cd03ab1f4629a8c64952a25f0c1baf6dc
Juanky <juancamiloherrera960@gmail.com>
2023-09-12 11:56:58 -0500

    Documentacion

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
+// Se obtienen la info de los botones
 const rain = document.getElementById("rain");
 const fire = document.getElementById("fire");
 const wind = document.getElementById("wind");
 const forest = document.getElementById("forest");
 
+// Se inicializan las iteraciones de cada botón
 let numRain = 1
 let numFire = 1
 let numWind = 1
 let numForest = 1
 
+// Se inicializan los audios de cada ruido
 const rainAudio = new Audio("./Noises/Rain.mp3");
 const fireAudio = new Audio("./Noises/Fire.mp3");
 const windAudio = new Audio("./Noises/Wind.mp3");
 const forestAudio = new Audio("./Noises/Forest.mp3");
 
+// Se configura cada audio como loop
 rainAudio.loop = true
 fireAudio.loop = true
 windAudio.loop = true
 forestAudio.loop = true
 
+// Escucha si dan click y dependiendo del boton ejecutan su función correspondiente
 rain.addEventListener("click", rainPlayer);
 fire.addEventListener("click", firePlayer);
 wind.addEventListener("click", windPlayer);
 forest.addEventListener("click", forestPlayer);
 
+// Funciones de para cada ruido
 function rainPlayer() {
     numRain++;
     
+    // if para saber si el número es par o impar, dependiendo reproduce o pausa el audio
+    // y usa la función toggleHover que le envía el nombre del ruido y su estado
     if (numRain % 2 == 0) {
         rainAudio.play();
         toggleHover("rain", true);
@@ -60,7 +68,6 @@ function windPlayer() {
 }
 
 function forestPlayer() {
-    forestAudio.loop = true
     numForest++;
 
     if (numForest % 2 == 0) {
@@ -72,6 +79,7 @@ function forestPlayer() {
     }
 }
 
+// Función que agrega o remueve la clase con la cual se ve el botón activado
 function toggleHover(name, stateHover) {
     const btn = document.getElementById(`${name}`);
 
